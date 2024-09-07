25dacf6b4b7f9e78c7d2c959d3adafd00b74783b
javier12s <151806272+javier12s@users.noreply.github.com>
2024-09-07 16:51:09 -0600

    Add files via upload

---
+++ tercer base.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+import numpy as np
+
+
+import pandas as pd 
+#Supongamos que 'lista python' es una lista de listas con datos para el Dataframe 
+lista python = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+
+
+filas  = ['a', 'b', 'c']
+columnas= [  'I ', 'II' , 'III', 'IV']
+
+
+df = pd.DataFrame(lista python, index-filas, columns-columnas)
+
+df
